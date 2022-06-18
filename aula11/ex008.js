f517f4b000b212b34a0ca078848585e02c6307f7
Elton Gonçalves Alves <egaautomacao@gmail.com>
2022-06-18 15:05:13 -0300

    Inicio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 23
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel>60) { //Condição simples
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija com segurança.`)
